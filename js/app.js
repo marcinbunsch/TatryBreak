e9720e3390397e36c7734286db1463e02e5f7219
Jarek Pelczynski <jpelczynski@gmail.com>
2014-12-10 17:35:37 +0100

    add current weather stats

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var loadConditions = function () {
+  var url = "http://www.test.tatrynet.pl/pogoda/weatherMiddleware_v1.0/xml/lokalizacje1.xml";
+
+  var degrees = { 'gor': {}, 'ps': {}, 'mo': {} };
+
+  $.ajax({
+    url: url,
+    dataType: "xml",
+    success: function(data) {
+      var data = data;
+      console.log(data);
+
+
+      degrees.gor.temp = $(data).find('#gor').find('temperatura').find('aktualna').text();
+      degrees.gor.wind = $(data).find('#gor').find('wiatr').find('silaMax').text();
+
+      degrees.ps.temp  = $(data).find('#ps').find('temperatura').find('aktualna').text();
+      degrees.ps.wind  = $(data).find('#ps').find('wiatr').find('silaMax').text();
+
+      degrees.mo.temp  = $(data).find('#mo').find('temperatura').find('aktualna').text();
+      degrees.mo.wind  = $(data).find('#mo').find('wiatr').find('silaMax').text();
+
+      appendTemp(degrees);
+    }
+  });
+
+};
+
+var appendTemp = function (degrees) {
+  $.each(degrees, function (k, v) {
+    console.log(k,v)
+    $('.' + k).append('<div class="condition"><i class="wi wi-thermometer"></i> ' + v.temp + ' °C &nbsp;&nbsp; <i class="wi wi-strong-wind"></i> ' + v.wind + 'm/s</div>');
+
+
+  });
+
+};
+
+loadConditions();
