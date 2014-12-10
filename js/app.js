13938444dbb1672f490dd2b65127fe76205b0303
Jarek Pelczynski <jpelczynski@gmail.com>
2014-12-10 19:02:16 +0100

    add information about avalanges from tpn.pl

--- js/app.js
+++ js/app.js
@@ -8,8 +8,6 @@ var loadConditions = function () {
     dataType: "xml",
     success: function(data) {
       var data = data;
-      console.log(data);
-
 
       degrees.gor.temp = $(data).find('#gor').find('temperatura').find('aktualna').text();
       degrees.gor.wind = $(data).find('#gor').find('wiatr').find('silaMax').text();
@@ -28,12 +26,24 @@ var loadConditions = function () {
 
 var appendTemp = function (degrees) {
   $.each(degrees, function (k, v) {
-    console.log(k,v)
     $('.' + k).append('<div class="condition"><i class="wi wi-thermometer"></i> ' + v.temp + ' °C &nbsp;&nbsp; <i class="wi wi-strong-wind"></i> ' + v.wind + 'm/s</div>');
-
-
   });
+};
 
+var loadAvalancheWarning = function () {
+  var warning = {};
+
+  $('#tmp').load('http://tpn.pl/zwiedzaj/komunikat-lawinowy .degree', function (response, status, xhr) {
+    warning.sign = $(response).find('.avalanche').find('img[src*="avalanche"]');
+    warning.sign = 'Stopień zagrożenia lawinowego: <img src="http://tpn.pl/themes/' + warning.sign[0].src.split('/themes')[1] + '">';
+
+    appendAvalancheWarning(warning.sign);
+  });
+};
+
+var appendAvalancheWarning = function (warning) {
+  $('#avalanches-warning').html(warning);
 };
 
 loadConditions();
+loadAvalancheWarning();
